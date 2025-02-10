ff46835008eb79dcb4c205607db26643b9174279
StephanoNaressi <naressistephano@outlook.com>
2025-02-10 18:42:23 +0000

    Create sfml window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include <optional>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(){
-    std::cout << "Hola" << std::endl;
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
+
     return 0;
 }
